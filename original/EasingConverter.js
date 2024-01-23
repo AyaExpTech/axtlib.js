08256d23e85fbed468456520049ff0ed1978c58a
AXT-AyaKoto <ayagawap@gmail.com>
2024-01-23 13:20:42 +0900

    v0.4.3 (制御点ほぼ違った、Copilotさん？)

--- original/EasingConverter.js
+++ original/EasingConverter.js
@@ -116,41 +116,41 @@ export default class EasingConverter {
             case "Linear_InOut":
                 return [0, 0, 1, 1];
             case "Sine_In":
-                return [0.47, 0, 0.745, 0.715];
+                return [0.12, 0, 0.39, 0];
             case "Sine_Out":
-                return [0.39, 0.575, 0.565, 1];
+                return [0.61, 1, 0.88, 1];
             case "Sine_InOut":
-                return [0.445, 0.05, 0.55, 0.95];
+                return [0.37, 0, 0.63, 1];
             case "Quad_In":
-                return [0.55, 0.085, 0.68, 0.53];
+                return [0.11, 0, 0.5, 0];
             case "Quad_Out":
-                return [0.25, 0.46, 0.45, 0.94];
+                return [0.5, 1, 0.89, 1];
             case "Quad_InOut":
-                return [0.455, 0.03, 0.515, 0.955];
+                return [0.45, 0, 0.55, 1];
             case "Cubic_In":
-                return [0.55, 0.055, 0.675, 0.19];
+                return [0.32, 0, 0.67, 0];
             case "Cubic_Out":
-                return [0.215, 0.61, 0.355, 1];
+                return [0.33, 1, 0.68, 1];
             case "Cubic_InOut":
-                return [0.645, 0.045, 0.355, 1];
+                return [0.65, 0, 0.35, 1];
             case "Quart_In":
-                return [0.895, 0.03, 0.685, 0.22];
+                return [0.5, 0, 0.75, 0];
             case "Quart_Out":
-                return [0.165, 0.84, 0.44, 1];
+                return [0.25, 1, 0.5, 1];
             case "Quart_InOut":
-                return [0.77, 0, 0.175, 1];
+                return [0.76, 0, 0.24, 1];
             case "Expo_In":
-                return [0.95, 0.05, 0.795, 0.035];
+                return [0.76, 0, 0.24, 1];
             case "Expo_Out":
-                return [0.19, 1, 0.22, 1];
+                return [0.16, 1, 0.3, 1];
             case "Expo_InOut":
                 return [0.87, 0, 0.13, 1];
             case "Circ_In":
-                return [0.6, 0.04, 0.98, 0.335];
+                return [0.55, 0, 1, 0.45];
             case "Circ_Out":
-                return [0.075, 0.82, 0.165, 1];
+                return [0, 0.55, 0.45, 1];
             case "Circ_InOut":
-                return [0.785, 0.135, 0.15, 0.86];
+                return [0.85, 0, 0.15, 1];
             default:
                 throw new Error("Easing function not found.");
         }
